ef2619e588f071c3567b178b28181671251f6054
Marcel Linden <linden@schulz.local>
2020-05-29 12:03:28 +0200

    Express Server created.

---
+++ server.js
@@ -0,0 +1,17 @@
+/*  import express and http modules. */
+let express = require('express');
+
+let app     = express();
+let server  = require('http').createServer(app);
+
+/* import Socket.io */
+let io = require('socket.io')(server);
+/* set port */
+let port = process.env.PORT || 8080;
+
+// start Webserver pn port 8080.
+server.listen(port, function () {
+    console.info('Webserver run and listening on port %d', port);
+});
+
+app.use(express.static(__dirname + '/public'));
